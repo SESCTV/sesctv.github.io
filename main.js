91772cdb173e1cbb4eeb6df992d1728400547b56
lambequeeugosto <lambequeeugosto@gmail.com>
2020-08-21 14:33:47 -0300

    Utilizando service worker script do Google (teste)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('https://developers.google.com/sw.js').then(function (registration) {
             console.log('Service worker successfully registered on scope', registration.scope);
         }).catch(function (error) {
             console.log('Service worker failed to register', error);
